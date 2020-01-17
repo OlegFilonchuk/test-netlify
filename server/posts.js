e5dd0f442d4b5fec0804af746ba2c4f367bf3964
OlegFilonchuk <oifilonchuk@gmail.com>
2020-01-17 11:47:54 +0200

    added md parsing on node js

--- server/posts.js
+++ server/posts.js
@@ -1,16 +1,27 @@
+const parseMd = require('front-matter-markdown');
 const fs = require('fs');
 const util = require('util');
 
 const readdir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
 
 const posts = async () => {
 
-  const files = await readdir(`${__dirname}/../content/blogPosts`, );
+  const fileNames = await readdir(`${__dirname}/../content/blogPosts`, );
 
-  return files.map((item) => ({
-    name: item,
-    slug: item
-  }))
+  const result = [];
+
+  for await (let fileName of fileNames) {
+    const fileContent = await readFile(`${__dirname}/../content/blogPosts/${fileName}`);
+    const data = await parseMd(fileContent);
+
+    result.push({
+      name: data.title,
+      slug: data.title
+    })
+  }
+
+  return result
 };
 
 module.exports = posts;
